cc4f11f315a1aca515a666501844f01fbaf4c1d6
abhi6983 <46106600+abhi6983@users.noreply.github.com>
2018-12-30 15:53:58 +0400

    Add files via upload

---
+++ counter_with_input.py
@@ -0,0 +1,46 @@
+# code by Abhijeet Ghosh
+# counts the numbers based on range and steps from user
+
+print("\t\tLet's get counting!!!")
+print("\nTo start, please let me know the range and and steps between numbers")
+print("\nAt any time to quit, just type 'exit'")
+
+#get the starting number and keep promting till a value is entered
+start = ""
+while not start:
+    start = input("\nWhat number should I start from?\n")
+    
+#quit if user enter 'exit'
+if start == "exit":
+    exit()
+    
+#get the stopping number and keep promting till a value is entered    
+stop = ""
+while not stop :
+    stop = input("\nWhat number should I stop at?\n")
+
+#quit if user enter 'exit'
+if stop == "exit":
+    exit()
+
+#get the gap number and keep promting till a value is entered     
+step = ""
+while not step:
+    step = input("\nWhat is the gap between the numbers?\n")
+
+#quit if user enter 'exit'
+if step == "exit":
+    exit()
+
+print("\nAll right, let the counting begin!")
+
+#sets up the counting range and prints the numbers
+start = int(start)
+stop = int(stop)
+step = int(step)
+
+count = range(start,stop+1,step)
+
+print(*count)
+
+input("\nPress Enter key to exit")
